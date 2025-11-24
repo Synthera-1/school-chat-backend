f60283ff44b25b6b850baeaa870c1a2b1da35054
MeCode <164209877+Synthera-1@users.noreply.github.com>
2025-11-24 11:09:54 -0600

    Create Message.js

---
+++ backend/models/Message.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    author: { type: String, required: true },
+    text: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Message", messageSchema);
